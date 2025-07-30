80eb82ee1a20a71594c6d728ec637ff037d1b454
Thomas Kunzler <105678691+TKunzler@users.noreply.github.com>
2025-07-30 11:08:09 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import streamlit as st
+
+# Importar Dados
+url_csv = 'https://docs.google.com/spreadsheets/d/1AQNVp1AlD3XqrOxILCCWa6ULgsW6ZLhpB5ODndTDrlg/export?format=csv&id=1AQNVp1AlD3XqrOxILCCWa6ULgsW6ZLhpB5ODndTDrlg&gid=0'
+df = pd.read_csv(url_csv)
+
+# Página
+st.title("Sorteio Campanha de Aniversário")
+
+st.table(df)
