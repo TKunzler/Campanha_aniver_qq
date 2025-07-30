24514279e34a4c21bc81cf0829f0162080091e16
Thomas Kunzler <105678691+TKunzler@users.noreply.github.com>
2025-07-30 13:32:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,23 @@ from io import BytesIO
 url_csv = 'https://docs.google.com/spreadsheets/d/1AQNVp1AlD3XqrOxILCCWa6ULgsW6ZLhpB5ODndTDrlg/export?format=csv&id=1AQNVp1AlD3XqrOxILCCWa6ULgsW6ZLhpB5ODndTDrlg&gid=0'
 df = pd.read_csv(url_csv)
 
+# Carrega e redimensiona
+image = Image.open("quero.png").resize((150, 150))
+
+# Converte imagem para base64
+buffered = BytesIO()
+image.save(buffered, format="PNG")
+img_str = base64.b64encode(buffered.getvalue()).decode()
+
+# Insere imagem centralizada com HTML
+st.markdown(
+    f"""
+    <div style="text-align: center;">
+        <img src="data:image/png;base64,{img_str}" width="150">
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 # Página
 st.title("Sorteio Campanha de Aniversário")
