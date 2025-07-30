a4a03166ab328fc486d36a1c38dcfd4d0a310b04
Thomas Kunzler <105678691+TKunzler@users.noreply.github.com>
2025-07-30 11:34:28 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 url_csv = 'https://docs.google.com/spreadsheets/d/1AQNVp1AlD3XqrOxILCCWa6ULgsW6ZLhpB5ODndTDrlg/export?format=csv&id=1AQNVp1AlD3XqrOxILCCWa6ULgsW6ZLhpB5ODndTDrlg&gid=0'
 df = pd.read_csv(url_csv)
 
-# Página
+# Página#
 st.title("Sorteio Campanha de Aniversário")
 
 st.table(df)
